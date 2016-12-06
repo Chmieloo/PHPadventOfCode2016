9dfa595fed56aa26ff7762848c7eee232eeda7c9
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2016-12-06 15:37:49 +0100

    AdventOfCode Puzzle.06 Part 1,2

---
+++ Day06/PuzzlePartOne.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Day06;
+
+/**
+ * Puzzle day 6
+ * Class PuzzlePartOne
+ * Advent Of Code 2016
+ */
+class PuzzlePartOne
+{
+    private $input;
+
+    private $message;
+
+    /**
+     * Puzzle constructor.
+     * Load the file into class variable
+     */
+    public function __construct()
+    {
+        // Put puzzle input into string
+        $this->input = file('input.txt');
+    }
+
+    /**
+     * @return int
+     */
+    public function processLines()
+    {
+        $solution = '';
+
+        $positionsCount = [
+            0 => [],
+            1 => [],
+            2 => [],
+            3 => [],
+            4 => [],
+            5 => [],
+            6 => [],
+            7 => [],
+        ];
+
+        foreach ($this->input as $line) {
+            $line = trim($line);
+            $letters = str_split($line);
+
+            foreach ($letters as $key => $letter) {
+                if (array_key_exists($letter, $positionsCount[$key])) {
+                    $positionsCount[$key][$letter]++;
+                } else {
+                    $positionsCount[$key][$letter] = 1;
+                }
+            }
+        }
+
+        foreach ($positionsCount as $countArray) {
+            $flippedArray = array_flip($countArray);
+            krsort($flippedArray);
+            $solution .= array_shift($flippedArray);
+        }
+
+        return $solution;
+    }
+}
+
+$puzzle = new PuzzlePartOne();
+$solution = $puzzle->processLines();
+echo $solution;
+
+
