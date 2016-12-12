c8fdf1fad5a2a997b39b80bcaa93bad4ef46582a
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2016-12-12 13:26:50 +0100

    Dirty solution day 12

--- Puzzles/Day12/Puzzle.php
+++ Puzzles/Day12/Puzzle.php
@@ -11,7 +11,12 @@ use Puzzles\Abstraction\Puzzle as PuzzleAbstract;
  */
 class Puzzle extends PuzzleAbstract
 {
-    protected $registers;
+    protected $registers = [
+        'a' => 0,
+        'b' => 0,
+        'c' => 1,
+        'd' => 0,
+    ];
     protected $pointer = 0;
 
     public function __construct()
@@ -29,82 +34,97 @@ class Puzzle extends PuzzleAbstract
 
     public function processInput()
     {
+        $j = 0;
         do {
             $this->processInstruction($this->pointer);
-            echo $this->pointer . PHP_EOL;
-        } while ($this->pointer <= count($this->input));
+            //echo $this->registers['a'] . ' ' . $this->registers['b'] . PHP_EOL;
+            $j++;
+        } while ($this->pointer < count($this->input));
 
+        echo $j . PHP_EOL;
         var_dump($this->registers);
     }
 
     private function processInstruction($pointer)
     {
-        echo 'Processing instruction: ' . $pointer . PHP_EOL;
+        //echo 'Processing instruction: ' . ($pointer + 1). PHP_EOL;
         $line = $this->input[$pointer];
         $data = explode(' ', $line);
 
         switch ($data[0]) {
             case 'cpy':
                 $this->instructionCopy($data);
+                $this->display();
                 $this->pointer++;
                 break;
             case 'inc':
                 $this->instructionIncrease($data);
+                $this->display();
                 $this->pointer++;
                 break;
             case 'dec':
                 $this->instructionDecrease($data);
+                $this->display();
                 $this->pointer++;
                 break;
             case 'jnz':
                 $this->instructionJump($data);
+                $this->display();
                 break;
         }
     }
 
     private function instructionCopy($instruction)
     {
-        $from = $instruction[1];
-        $to = $instruction[2];
-
-        preg_match('/([a-z])+/', $from, $matchesLettersFrom);
-        preg_match('/(\d+)/', $from, $matchesDigitsFrom);
-
-        preg_match('/([a-z])+/', $from, $matchesLettersTo);
-        preg_match('/(\d+)/', $from, $matchesDigitsTo);
+        $from = trim($instruction[1]);
+        $to = trim($instruction[2]);
 
         # integer to register
-        if ($matchesDigitsFrom[0]) {
-            $this->registers[$to] = (int) $from;
-        }
-        if ($matchesLettersFrom[0]) {
+        if (is_numeric($from)) {
+            $this->registers[$to] = $from;
+        } else {
             $this->registers[$to] = $this->registers[$from];
         }
     }
 
     private function instructionIncrease($instruction)
     {
-        $key = $instruction[1];
+        $key = trim($instruction[1]);
         $this->registers[$key]++;
     }
 
     private function instructionDecrease($instruction)
     {
-        $key = $instruction[1];
+        $key = trim($instruction[1]);
         $this->registers[$key]--;
     }
 
     private function instructionJump($instruction)
     {
-        $targetPosition = $instruction[2];
-        if ($this->registers[$instruction[1]]) {
-            # Move pointer
-            $this->pointer = $this->pointer + (int) $targetPosition;
-            if ($targetPosition < 0)
-                echo $this->pointer . PHP_EOL;
+        $value = trim($instruction[1]);
+        $to = trim($instruction[2]);
+
+        # integer to register
+        if (is_numeric($value)) {
+            # Check if it is 0
+            if ($value == 0) {
+                $this->pointer++;
+            } else {
+                $this->pointer = $this->pointer + $to;
+            }
         } else {
-            $this->pointer++;
+            $registryValue = $this->registers[$value];
+            if ($registryValue == 0) {
+                $this->pointer++;
+            } else {
+                $this->pointer = $this->pointer + $to;
+            }
         }
+    }
+
+    private function display()
+    {
+        //var_dump($this->registers);
     }
 
     /**
